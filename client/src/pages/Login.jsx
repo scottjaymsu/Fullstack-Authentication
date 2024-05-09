9a947de309b4afde0a8a3b324b6b54453a77f3a5
Jay <scottjay@msu.edu>
2024-05-09 17:12:20 -0400

    Register and login pages are functional - Both need CSS styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { useState } from 'react';
 
 export default function Login() {
-  
+  /* Data is initialized to empty strings since form is not filled out until User enters information */
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+
   const loginUser = (e) => {
     e.preventDefault()
   }
@@ -10,9 +15,9 @@ export default function Login() {
     <div>
       <form>
         <label onSubmit={loginUser}>Email</label>
-        <input type="email" placeholder='enter email...' />
+        <input type="email" placeholder='enter email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})} /> 
         <label>Password</label>
-        <input type="password" placeholder='enter password...' />
+        <input type="password" placeholder='enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />        
         <button type="submit">Submit</button>
       </form>
     </div>
