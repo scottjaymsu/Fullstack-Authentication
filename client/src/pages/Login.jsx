5c3c5e5c4ba096557dcd8d1455dc8facbc94c235
Jay <scottjay@msu.edu>
2024-05-09 16:52:01 -0400

    Login page form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function Login() {
+  
+  const loginUser = (e) => {
+    e.preventDefault()
+  }
+  
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <label onSubmit={loginUser}>Email</label>
+        <input type="email" placeholder='enter email...' />
+        <label>Password</label>
+        <input type="password" placeholder='enter password...' />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
