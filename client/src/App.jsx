cd5daf569939e6b7a3bc2f52176eb47c3a8cfea6
Jay <scottjay@msu.edu>
2024-05-09 16:43:56 -0400

    nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Register from './pages/Register'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
