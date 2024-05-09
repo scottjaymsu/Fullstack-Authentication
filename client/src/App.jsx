47c0fafd288aea1da3e8a7e22c73dfd5bc82d0f9
Jay <scottjay@msu.edu>
2024-05-09 16:35:47 -0400

    react router dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/login' element={<Login />}/>
+    </Routes>
     </>
   )
 }
